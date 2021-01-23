028295829f494453f94abbcf7566a46d3905e49a
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2021-01-24 04:47:33 +0600

    adding cart functionality in webpage

--- main.js
+++ main.js
@@ -16,12 +16,25 @@ function cartItem(id, pname) {
     document.getElementById("cart").innerHTML = JSON.parse(localStorage.getItem("cart")).length
 }
 
+function removeItem(id) {
+    newcart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : []
+    newcart.map((item, index) => {
+        if (item.id == id) {
+            newcart.splice(index, 1)
+        }
+    })
+    localStorage.setItem("cart", JSON.stringify(newcart))
+    document.getElementById("cart").innerHTML = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")).length : [].length
+
+    document.querySelector('.cartpage').innerHTML = ""
+    cartsection()
+
+}
+
 function cartsection() {
     var mycard = document.querySelector('.cartpage')
-    newcart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : [] 
-    // alert('xxx')
+    newcart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : []
     for (let j = 1; j <= newcart.length; j++) {
-        console.log(j);
         var element = document.createElement("div")
         element.classList.add('card');
         element.setAttribute("style", "width: 18rem; width: 18rem;")
@@ -30,17 +43,23 @@ function cartsection() {
         var imgelement = document.createElement('img')
         imgelement.src = `./images/${newcart[j - 1].id}.png`
         imgelement.classList.add("img-fluid", "w-100", "h-50")
-        var hr = document.createElement('hr')
+        var hr1 = document.createElement('hr')
+        var hr2 = document.createElement('hr')
         var cardtitle = document.createElement('h5')
         cardtitle.classList.add('card-title')
         var text = document.createTextNode(`${newcart[j - 1].pname}`);
         cardtitle.appendChild(text)
         element1.appendChild(imgelement)
-        element1.appendChild(hr)
+        element1.appendChild(hr1)
         element1.appendChild(cardtitle)
-        element1.appendChild(hr)
+        element1.appendChild(hr2)
+        var btn = document.createElement("button")
+        var btntext = document.createTextNode("Remove")
+        btn.classList.add("btn", "btn-danger")
+        btn.appendChild(btntext)
+        btn.setAttribute('onclick', `removeItem(${newcart[j - 1].id})`)
+        element1.appendChild(btn)
         element.appendChild(element1)
         mycard.appendChild(element)
-        console.log(element);
     }
 }
